2c4a9fe703e02385416e56b7425dacfcc039919e
Mostafizur <mostafizzfuad@gmail.com>
2026-01-24 18:50:12 +0600

    feat: fetch and display post meta data

---
+++ app/components/PostCard.tsx
@@ -0,0 +1,27 @@
+import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+
+const PostCard = ({ post }: { post: PostMeta }) => {
+	return (
+		<article
+			key={post.slug}
+			className="bg-gray-800 p-6 rounded-lg shadow border border-gray-700"
+		>
+			<h3 className="text-2xl font-semibold text-blue-400 mb-2">
+				{post.title}
+			</h3>
+			<p className="text-sm text-gray-400 mb-3">
+				{new Date(post.date).toLocaleDateString()}
+			</p>
+			<p className="text-gray-300 mb-4 line-clamp-2">{post.excerpt}</p>
+			<Link
+				to={`/blog/${post.slug}`}
+				className="text-blue-300 hover:text-blue-200 hover:underline text-sm font-medium"
+			>
+				Read More →
+			</Link>
+		</article>
+	);
+};
+
+export default PostCard;
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,36 @@
-const BlogPage = () => {
+import type { Route } from "./+types/index";
+import type { PostMeta } from "~/types";
+import PostCard from "~/components/PostCard";
+
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+	const url = new URL("/posts-meta.json", request.url);
+	const res = await fetch(url.href);
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch posts");
+	}
+
+	const data = await res.json();
+	return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+	const { posts } = loaderData;
+	// console.log(posts);
+
 	return (
-		<>
+		<section className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
 			<h2 className="text-3xl font-bold mb-8 text-white">📝 Blog</h2>
-		</>
+
+			<div className="grid gap-6">
+				{posts.map((post) => (
+					<PostCard key={post.id} post={post} />
+				))}
+			</div>
+		</section>
 	);
 };
+
 export default BlogPage;
