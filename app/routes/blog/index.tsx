1f88af6c0b018d9ee18b126db4c542890722aef1
Mostafizur <mostafizzfuad@gmail.com>
2026-01-25 00:54:41 +0600

    feat: sort posts by date and add reusable pagination

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "~/components/PostCard";
+import Pagination from "~/components/Pagination";
 
 export async function loader({
 	request,
@@ -12,22 +14,49 @@ export async function loader({
 		throw new Error("Failed to fetch posts");
 	}
 
-	const data = await res.json();
+	const data: PostMeta[] = await res.json();
+
+	// ডেট অনুযায়ী সর্ট করা (Newest First)
+	data.sort((a, b) => {
+		return new Date(b.date).getTime() - new Date(a.date).getTime();
+	});
+
 	return { posts: data };
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
 	const { posts } = loaderData;
-	// console.log(posts);
+
+	// ১. স্টেট এবং কনফিগারেশন
+	const [currentPage, setCurrentPage] = useState(1);
+	const postsPerPage = 3;
+
+	// ২. ক্যালকুলেশন
+	const totalPages = Math.ceil(posts.length / postsPerPage);
+	const indexOfLast = currentPage * postsPerPage;
+	const indexOfFirst = indexOfLast - postsPerPage;
+	const currentPosts = posts.slice(indexOfFirst, indexOfLast);
 
 	return (
 		<section className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
 			<h2 className="text-3xl font-bold mb-8 text-white">📝 Blog</h2>
 
 			<div className="grid gap-6">
-				{posts.map((post) => (
-					<PostCard key={post.id} post={post} />
-				))}
+				{/* পোস্ট লিস্ট */}
+				<div className="space-y-8">
+					{currentPosts.map((post) => (
+						<PostCard key={post.slug} post={post} />
+					))}
+				</div>
+
+				{/* পেজিনেশন কম্পোনেন্ট */}
+				{totalPages > 1 && (
+					<Pagination
+						currentPage={currentPage}
+						totalPages={totalPages}
+						onPageChange={(page) => setCurrentPage(page)}
+					/>
+				)}
 			</div>
 		</section>
 	);
